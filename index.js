dbb914a5749f3ca8eb939dd3c3cc1cec180449f4
dvsadovskyi_LOCAL_WORK <dvsadovskyi>
2023-06-23 17:37:06 +0300

    init mrkp

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 function App() {
     return (
-        <div>Hello world!</div>
+        <div className="container">
+            <div className="calculator">
+                <div id="display"></div>
+                <div className="buttons">
+                    <div id="clear">AC</div>
+                    <div id="divide">/</div>
+                    <div id="multiply">X</div>
+                    <div id="seven">7</div>
+                    <div id="eight">8</div>
+                    <div id="nine">9</div>
+                    <div id="subtract">-</div>
+                    <div id="4">4</div>
+                    <div id="5">5</div>
+                    <div id="6">6</div>
+                    <div id="add">+</div>
+                    <div id="1">1</div>
+                    <div id="2">2</div>
+                    <div id="3">3</div>
+                    <div id="equals">=</div>
+                    <div id="zero">0</div>
+                    <div id="decimal">.</div>
+                </div>
+            </div>
+        </div>
     )
 }
 
